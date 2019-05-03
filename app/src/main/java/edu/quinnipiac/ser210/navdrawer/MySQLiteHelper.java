0e9c7318d4cef74b965e50fbb8b45885104ef677
Brian Carballo <brian.carballo@quinnipiac.edu>
2019-05-03 18:48:40 -0400

    -Fixed Async Task Bug

--- app/src/main/java/edu/quinnipiac/ser210/navdrawer/MySQLiteHelper.java
+++ app/src/main/java/edu/quinnipiac/ser210/navdrawer/MySQLiteHelper.java
@@ -20,7 +20,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     public static final String COLUMN_QUANTITY = "quantity";
 
     public static final String DATABASE_NAME = "cryptodb";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 4;
 
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
--- app/src/main/java/edu/quinnipiac/ser210/navdrawer/ResultActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/navdrawer/ResultActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
